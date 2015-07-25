a6e890606269715574676edad3ebb55deca8e19e
spautz <spautz@web.de>
2015-07-25 10:59:11 +0100

    prepare test cases

---
+++ test/SmlMessageBody_Spec.js
@@ -0,0 +1,13 @@
+var chai = require('chai');
+chai.use(require('chai-change'));
+
+var expect = require("chai").expect;
+var should = require('chai').should;
+
+describe("SmlMessageBody", function() {
+
+	var SmlMessage = require("../lib/SmlMessageBody");
+	var Constants = require('../lib/Constants');
+		   
+	
+});
---
+++ test/SmlMessage_Spec.js
@@ -0,0 +1,25 @@
+var chai = require('chai');
+chai.use(require('chai-change'));
+
+var expect = require("chai").expect;
+var should = require('chai').should;
+
+describe("SmlMessage", function() {
+
+	var SmlMessage = require("../lib/SmlMessage");
+	var Constants = require('../lib/Constants');
+		   
+	describe("SmlMessage()", function(){
+		  
+	});
+	
+	describe("write()", function(){	  
+	});
+	
+	describe("getSize()", function(){	  
+	});
+	
+	describe("parse()", function(){	  
+	});
+	
+});
